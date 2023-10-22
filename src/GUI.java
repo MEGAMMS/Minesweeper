94bbc3f9133d32c15f365ba0523e3020d500c2b0
Mouaaz Sweid <mmsweid@gmail.com>
2023-10-23 00:29:29 +0300

    implment grid

--- src/GUI.java
+++ src/GUI.java
@@ -3,10 +3,17 @@ import java.util.*;
 import java.awt.*;
 
 public class GUI extends JFrame {
+    // set up
+    public static final int WIDTH = 50;
+    public static final int HEIGHT = 50;
+    public static final int X = 16;
+    public static final int Y = 9;
+    public static final int SPACING = 4;
+
     public GUI() {
         this.setTitle("Minesweeper");
         // this.setSize(1286, 829); //Original Vals 1280+6, 800+29
-        this.setSize(800, 600);
+        this.setSize(X * WIDTH + 13, Y * HEIGHT + 37);
         this.setBackground(Color.DARK_GRAY);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
@@ -17,8 +24,13 @@ public class GUI extends JFrame {
 
     public class Board extends JPanel {
         public void paintComponent(Graphics g) {
-            g.setColor(Color.red);
-            g.fillRect(0, 0, 80, 80);
+            g.setColor(Color.gray);
+            for (int i = 0; i < X; i++) {
+                for (int j = 0; j < Y; j++) {
+                    g.fillRect(SPACING + i * GUI.WIDTH, SPACING + (j + 1) * GUI.HEIGHT,
+                            GUI.WIDTH - 2 * SPACING, GUI.HEIGHT - 2 * SPACING);
+                }
+            }
         }
     }
 }
